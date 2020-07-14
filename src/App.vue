2a21adaf0ac6faabcf2bb36beac020b6b0aac834
PatrycjaDom <patcho2808@gmail.com>
2020-07-14 09:44:39 +0200

    Added form with datepickers

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <div class="container">
       <Info></Info>
+      <Form></Form>
     </div>
   </div>
 </template>
 
 <script>
 import Info from "./components/Info.vue";
+import Form from "@/components/Form";
 
 export default {
   name: 'App',
   components: {
+    Form,
     Info
   }
 }
---
+++ src/components/Form.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <b-form>
+            <label>Title: </label>
+            <b-form-input v-model="title" placeholder="How to fly"></b-form-input>
+            <small> It will be the name of the event in the calendar </small>
+            <br>
+            <br>
+            <label> When did you learn that stuff: </label>
+            <b-form-datepicker @input="computeRepetitions" v-model="initialDate" ></b-form-datepicker>
+            <br>
+            <br>
+            <label> First repetition: </label>
+            <b-form-datepicker v-model="repetitions[0]" ></b-form-datepicker>
+            <br>
+            <br>
+            <label> Second repetition: </label>
+            <b-form-datepicker v-model="repetitions[1]" ></b-form-datepicker>
+            <br>
+            <br>
+            <label> Third repetition: </label>
+            <b-form-datepicker v-model="repetitions[2]" ></b-form-datepicker>
+            <br>
+            <br>
+            <label> Fourth repetition: </label>
+            <b-form-datepicker v-model="repetitions[3]" ></b-form-datepicker>
+            <br>
+            <br>
+        </b-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Form",
+        data: function(){
+            return {
+                title: null,
+                initialDate: new Date(),
+                repetitions: []
+            }
+        },
+        methods: {
+          computeRepetitions: function(){
+              this.repetitions = [];
+              for(let i=0; i<4; i++){
+                  this.repetitions.push(new Date(this.initialDate));
+              }
+              this.repetitions[0].setDate(this.repetitions[0].getDate() + 1);
+              this.repetitions[1].setDate(this.repetitions[1].getDate() + 10);
+              this.repetitions[2].setDate(this.repetitions[2].getDate() + 30);
+              this.repetitions[3].setDate(this.repetitions[3].getDate() + 60);
+          }
+        },
+        mounted: function(){
+            this.computeRepetitions();
+        }
+    }
+</script>
